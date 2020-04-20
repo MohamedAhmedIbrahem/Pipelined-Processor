0b435cac5373d349bb5b99e5224af48728d0c636
MohamedAhmedIbrahem <32310296+MohamedAhmedIbrahem@users.noreply.github.com>
2020-04-20 23:39:01 +0200

    Update ALU.vhdl

--- ALU/ALU.vhdl
+++ ALU/ALU.vhdl
@@ -16,38 +16,40 @@ ARCHITECTURE arch OF ALU IS
 BEGIN 
     PROCESS(A,B,OP)
     VARIABLE RES : STD_LOGIC_VECTOR(31 DOWNTO 0);
-    VARIABLE CARRY : STD_LOGIC;
+    VARIABLE CARRY,IS_LDM : STD_LOGIC;
     BEGIN 
 	RES   := (OTHERS => '0');
 	CARRY := '0';
-        IF OP = "0010" THEN		-- NOT
-            RES := NOT A;
-        ELSIF OP = "0011" THEN	-- INC
-            RES := A + 1;
-        ELSIF OP = "0100" THEN	-- DEC
-            RES := A - 1;
-        ELSIF OP = "0101" THEN	-- ADD
-            RES := A + B;
-        ELSIF OP = "0110" THEN	-- SUB
-            RES := A - B;
-        ELSIF OP = "0111" THEN	-- AND
-            RES := A AND B;
-        ELSIF OP = "1000" THEN	-- OR
-            RES := A OR B;
-        ELSIF OP = "1001" THEN  -- SHL
-            RES := STD_LOGIC_VECTOR(UNSIGNED(A) SLL TO_INTEGER(UNSIGNED(B)));
-            CARRY := A(32 - TO_INTEGER(UNSIGNED(B)));
-        ELSIF OP = "1010" THEN  -- SHR
-            RES := STD_LOGIC_VECTOR(UNSIGNED(A) SRL TO_INTEGER(UNSIGNED(B)));
-            CARRY := A(TO_INTEGER(UNSIGNED(B)) - 1);
-        END IF;
-        C <= RES;
-	IF RES = x"00000000" THEN
-	    FLAGS(0) <= '1';
-	ELSE 
-	    FLAGS(0) <= '0';
+	IF OP = "0010" THEN		-- NOT
+		RES := NOT A;
+	ELSIF OP = "0011" THEN	-- INC
+		RES := A + 1;
+	ELSIF OP = "0100" THEN	-- DEC
+		RES := A - 1;
+	ELSIF OP = "0101" THEN	-- ADD
+		RES := A + B;
+	ELSIF OP = "0110" THEN	-- SUB
+		RES := A - B;
+	ELSIF OP = "0111" THEN	-- AND
+		RES := A AND B;
+	ELSIF OP = "1000" THEN	-- OR
+		RES := A OR B;
+	ELSIF OP = "1001" THEN  -- SHL
+		RES := STD_LOGIC_VECTOR(UNSIGNED(A) SLL TO_INTEGER(UNSIGNED(B)));
+		CARRY := A(32 - TO_INTEGER(UNSIGNED(B)));
+	ELSIF OP = "1010" THEN  -- SHR
+		RES := STD_LOGIC_VECTOR(UNSIGNED(A) SRL TO_INTEGER(UNSIGNED(B)));
+		CARRY := A(TO_INTEGER(UNSIGNED(B)) - 1);
+	ELSIF OP = "1011" THEN  -- LDM
+		RES := B;
 	END IF;
-        FLAGS(1) <= RES(31);
-        FLAGS(2) <= CARRY;
+	C <= RES;
+	IF RES = x"00000000" THEN
+		FLAGS(0) <= '1';
+	ELSE 
+		FLAGS(0) <= '0';
+	END IF;
+		FLAGS(1) <= RES(31);
+		FLAGS(2) <= CARRY;
     END PROCESS;
 END ARCHITECTURE;
