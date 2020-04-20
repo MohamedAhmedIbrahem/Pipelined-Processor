e7e8ebe8c5a4b6130ba08f031eb90100a67ba4c7
MohamedAhmedIbrahem <32310296+MohamedAhmedIbrahem@users.noreply.github.com>
2020-04-21 01:10:55 +0200

    Update DC_EX_BUFFER.vhdl

--- Stages_Buffers/DC_EX_BUFFER.vhdl
+++ Stages_Buffers/DC_EX_BUFFER.vhdl
@@ -3,15 +3,15 @@ USE IEEE.STD_LOGIC_1164.ALL;
 
 ENTITY DC_EX_BUFFER IS
     PORT(
-        CLK, RST, EN                                    							: IN   STD_LOGIC;
-        Op2,Op1                                         							: IN   STD_LOGIC_VECTOR(31 DOWNTO 0);    -- operands
-        ALU_Op                                          							: IN   STD_LOGIC_VECTOR(3 DOWNTO 0) ;    -- ALU operation
-        SRC1,SRC2,DST1,DST2                             							: IN   STD_LOGIC_VECTOR(2 DOWNTO 0) ;    -- Registers' Adresses
-        WB1,WB2,WR,RD,I_O,PCWB,FLAGSWB,IS_SRC1,IS_SRC2  							: IN   STD_LOGIC					;    -- Signals
-        Op2_EX,Op1_EX                                         						: OUT  STD_LOGIC_VECTOR(31 DOWNTO 0);    -- operands
-        ALU_Op_EX                                          							: OUT  STD_LOGIC_VECTOR(3 DOWNTO 0) ;    -- ALU operation
-        SRC1_EX,SRC2_EX,DST1_EX,DST2_EX                             				: OUT  STD_LOGIC_VECTOR(2 DOWNTO 0) ;    -- Registers' Adresses
-        WB1_EX,WB2_EX,WR_EX,RD_EX,I_O_EX,PCWB_EX,FLAGSWB_EX,IS_SRC1_EX,IS_SRC2_EX  	: OUT  STD_LOGIC                         -- Signals
+        CLK, RST, EN                                    											: IN   STD_LOGIC;
+        Op2,Op1                                         											: IN   STD_LOGIC_VECTOR(31 DOWNTO 0);    -- operands
+        ALU_Op                                          											: IN   STD_LOGIC_VECTOR(3 DOWNTO 0) ;    -- ALU operation
+        SRC1,SRC2,DST1,DST2                             											: IN   STD_LOGIC_VECTOR(2 DOWNTO 0) ;    -- Registers' Adresses
+        WB1,WB2,WR,RD,I_O,PCWB,FLAGSWB,FLAGS_UPD,IS_SRC1,IS_SRC2  									: IN   STD_LOGIC					;    -- Signals
+        Op2_EX,Op1_EX                                         										: OUT  STD_LOGIC_VECTOR(31 DOWNTO 0);    -- operands
+        ALU_Op_EX                                          											: OUT  STD_LOGIC_VECTOR(3 DOWNTO 0) ;    -- ALU operation
+        SRC1_EX,SRC2_EX,DST1_EX,DST2_EX                             								: OUT  STD_LOGIC_VECTOR(2 DOWNTO 0) ;    -- Registers' Adresses
+        WB1_EX,WB2_EX,WR_EX,RD_EX,I_O_EX,PCWB_EX,FLAGSWB_EX,FLAGS_UPD_EX,IS_SRC1_EX,IS_SRC2_EX  	: OUT  STD_LOGIC                         -- Signals
     );
 END ENTITY;
 
@@ -45,6 +45,7 @@ BEGIN
 				FLAGSWB_EX 	<= '0';
 				IS_SRC1_EX 	<= '0';
 				IS_SRC2_EX 	<= '0';
+				FLAGS_UPD_EX<= '0';
             ELSIF EN='1' THEN
                 WB1_EX 		<= WB1;
 				WB2_EX 		<= WB2;
@@ -55,6 +56,7 @@ BEGIN
 				FLAGSWB_EX 	<= FLAGSWB;
 				IS_SRC1_EX 	<= IS_SRC1;
 				IS_SRC2_EX 	<= IS_SRC2;
+				FLAGS_UPD_EX<= FLAGS_UPD;
             END IF;
         END IF;
     END PROCESS;
