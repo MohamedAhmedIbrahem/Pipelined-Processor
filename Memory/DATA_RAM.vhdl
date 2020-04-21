57b7ead9221d5f5869174dbe9f3501bb5459bd7e
MohamedAhmedIbrahem <32310296+MohamedAhmedIbrahem@users.noreply.github.com>
2020-04-21 02:17:02 +0200

    Update DATA_RAM.vhdl

--- Memory/DATA_RAM.vhdl
+++ Memory/DATA_RAM.vhdl
@@ -8,6 +8,7 @@ ENTITY DATA_RAM IS
         CLK             : IN  STD_LOGIC;
         WR              : IN  STD_LOGIC;
         RD              : IN  STD_LOGIC;
+		RST				: IN  STD_LOGIC;
         Address         : IN  STD_LOGIC_VECTOR(AddressWidth-1 DOWNTO 0);
         Din             : IN  STD_LOGIC_VECTOR(2*CellSize-1 DOWNTO 0);
         Dout            : OUT STD_LOGIC_VECTOR(2*CellSize-1 DOWNTO 0)
@@ -18,9 +19,12 @@ ARCHITECTURE arch_DATA_RAM OF DATA_RAM IS
     TYPE memory IS ARRAY(0 TO (2**AddressWidth)-1) OF STD_LOGIC_VECTOR(CellSize-1 DOWNTO 0);
     SIGNAL Mem : memory := (OTHERS => "0000000000000000");
 BEGIN
-    PROCESS(CLK)
+    PROCESS(CLK,RST)
     BEGIN
-        IF RISING_EDGE(CLK) THEN
+		IF RST = '1' THEN
+			Mem <= (OTHERS => (OTHERS => '0'));
+		END IF;
+        IF RISING_EDGE(CLK) and RST = '0' THEN
             IF WR='1' and RD='0' THEN
                 Mem(TO_INTEGER(UNSIGNED(Address))) <= Din(2*CellSize-1 DOWNTO CellSize);
                 Mem(TO_INTEGER(UNSIGNED(Address)) + 1) <= Din(CellSize-1 DOWNTO 0);
@@ -29,4 +33,4 @@ BEGIN
             END IF;
         END IF;
     END PROCESS;
-END ARCHITECTURE;
+END ARCHITECTURE arch_DATA_RAM;
