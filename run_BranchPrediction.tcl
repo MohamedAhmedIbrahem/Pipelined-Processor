4d995679095e21e70925e035fc705ce2aadc3076
AhmedAfifi98 <Ahmed.Afifi98@eng-st.cu.edu.eg>
2020-05-11 22:11:28 +0200

    Add test cases with their do files

--- run_BranchPrediction.tcl
+++ run_BranchPrediction.tcl
@@ -1,7 +1,7 @@
 #!/usr/bin/tclsh
 
 ### Parameters
-set code_file_path "Test Cases/OneOperand.asm"; list
+set code_file_path "TestCases/BranchPrediction.asm"; list
 set memory_file_path "ram.txt"; list
 set memory_sim_path "CPU/memory_controller/main_memory/Mem"; list
 
@@ -10,24 +10,28 @@ if {[catch { exec python Assembler/assembler.py $code_file_path $memory_file_pat
     exec python3 Assembler/assembler.py $code_file_path $memory_file_path
 }
 
-
 ### Initialize simulation
 vsim CPU  
-add wave -unsigned *
+#add wave -unsigned *
 #add wave -unsigned CPU/Fetch_Stage/fetch_forwarding_unit/*
 #add wave -unsigned CPU/Execute_Stage/EX_FORW_UNIT/*
-add wave -decimal memory_controller/main_memory/Mem
-add wave -decimal memory_controller/instruction_cache/cache
-add wave -decimal memory_controller/data_cache/cache
-add wave -unsigned Decode_Stage/registers/Register_File
-
+add wave CLK
+add wave RST
+add wave INT
+add wave -hexadecimal memory_controller/main_memory/Mem
+add wave -hexadecimal memory_controller/instruction_cache/cache
+add wave -hexadecimal memory_controller/data_cache/cache
+add wave -hexadecimal Decode_Stage/registers/Register_File
+add wave -hexadecimal Input_Port 
+add wave -hexadecimal Output_Port
+add wave flags
 # Remove numeric std warnings before initialization
 set NumericStdNoWarnings 1; list
 run 0
 set NumericStdNoWarnings 0; list
 
 ############################
-add wave -radix unsigned Fetch_Stage/pc_out
+add wave -hexadecimal Fetch_Stage/pc_out
 #add wave -radix unsigned Fetch_Stage/pc_in
 #add wave Fetch_Stage/pc_enable
 #add wave Fetch_Stage/instruction_read
@@ -50,7 +54,7 @@ mem load -filldata $ram_contents $memory_sim_path -endaddress $end_address
 force CLK 1 0, 0 50 -r 100
 force RST 1
 force INT 0
-force Input_Port 10#17
+force Input_Port 16#0
 run 100
 force RST 0
 run 2000
