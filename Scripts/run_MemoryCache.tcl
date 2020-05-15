bab536aa02b8691e14e7095907602f56a5a7bde8
Yosry Ahmed <yosrym93@gmail.com>
2020-05-15 02:34:34 +0200

    Updated test cases do files

--- Scripts/run_MemoryCache.tcl
+++ Scripts/run_MemoryCache.tcl
@@ -12,35 +12,20 @@ if {[catch { exec python Assembler/assembler.py $code_file_path $memory_file_pat
 
 ### Initialize simulation
 vsim CPU  
-#add wave -unsigned *
-#add wave -unsigned CPU/Fetch_Stage/fetch_forwarding_unit/*
-#add wave -unsigned CPU/Execute_Stage/EX_FORW_UNIT/*
 add wave CLK
 add wave RST
 add wave INT
-add wave -hexadecimal memory_controller/main_memory/Mem
-add wave -hexadecimal memory_controller/instruction_cache/cache
-add wave -hexadecimal memory_controller/data_cache/cache
-add wave -hexadecimal memory_controller/current_state
-add wave -hexadecimal Decode_Stage/registers/Register_File
+add wave -label registers_file -hexadecimal Decode_Stage/registers/Register_File
 add wave -hexadecimal Input_Port 
 add wave -hexadecimal Output_Port
 add wave flags
+add wave -label PC -hexadecimal Fetch_Stage/pc_out
+add wave -label SP -hexadecimal { Decode_stage/sp_data_out (10 DOWNTO 0) }
+
 # Remove numeric std warnings before initialization
 set NumericStdNoWarnings 1; list
 run 0
 set NumericStdNoWarnings 0; list
-
-############################
-add wave -label PC -hexadecimal Fetch_Stage/pc_out
-add wave -label SP -hexadecimal { Decode_stage/sp_data_out (10 DOWNTO 0) }
-#add wave -radix unsigned Fetch_Stage/pc_in
-#add wave Fetch_Stage/pc_enable
-#add wave Fetch_Stage/instruction_read
-#add wave Fetch_Stage/RET_Fetch
-#add wave Fetch_Stage/int_internal
-#add wave Execute_Stage/*
-############################
 
 ### Load data memory
 set ram_contents_file [open $memory_file_path]; list
---
+++ read_me.txt
@@ -0,0 +1,8 @@
+To run any test case:
+
+1. Create project and add all vhdl files.
+2. Compile all files 3 times to resolve dependencies.
+3. do Scripts/run_<case>.tcl
+
+This runs the assembler to generate the memory text file, loads the file and runs the appropriate test case.
+The memory text files are provided just in case.
